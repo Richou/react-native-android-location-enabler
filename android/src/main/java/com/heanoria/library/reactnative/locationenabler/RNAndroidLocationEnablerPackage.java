0904158f64045232af150c2ea3daaa8cc1150276
Richard <richou6@gmail.com>
2017-09-02 16:18:30 +0200

    Add createJSModules method without override for retro compatibility

--- android/src/main/java/com/heanoria/library/reactnative/locationenabler/RNAndroidLocationEnablerPackage.java
+++ android/src/main/java/com/heanoria/library/reactnative/locationenabler/RNAndroidLocationEnablerPackage.java
@@ -20,4 +20,8 @@ public class RNAndroidLocationEnablerPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
+    }
 }
