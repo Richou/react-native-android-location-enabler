29b0b40858b0beacd37e58d996134852dd954263
Richou <richou6@gmail.com>
2019-06-15 22:13:25 +0200

    Update play services version and code

--- android/src/main/java/com/heanoria/library/reactnative/locationenabler/RNAndroidLocationEnablerModule.java
+++ android/src/main/java/com/heanoria/library/reactnative/locationenabler/RNAndroidLocationEnablerModule.java
@@ -3,9 +3,7 @@ package com.heanoria.library.reactnative.locationenabler;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
@@ -14,16 +12,15 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -34,7 +31,7 @@ import static android.app.Activity.RESULT_OK;
  * @author Richou
  * @since 2016-12
  */
-public class RNAndroidLocationEnablerModule extends ReactContextBaseJavaModule implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<LocationSettingsResult>, ActivityEventListener {
+public class RNAndroidLocationEnablerModule extends ReactContextBaseJavaModule implements ActivityEventListener, OnCompleteListener<LocationSettingsResponse> {
 
     private static final String SELF_MODULE_NAME = "RNAndroidLocationEnabler";
     private static final String LOCATION_INTERVAL_DURATION_PARAMS_KEY = "interval";
@@ -47,9 +44,8 @@ public class RNAndroidLocationEnablerModule extends ReactContextBaseJavaModule i
     private static final String ERR_USER_DENIED_CODE = "ERR00";
     private static final String ERR_SETTINGS_CHANGE_UNAVAILABLE_CODE = "ERR01";
     private static final String ERR_FAILED_OPEN_DIALOG_CODE = "ERR02";
+    private static final String ERR_INTERNAL_ERROR = "ERR03";
 
-    private GoogleApiClient googleApiClient;
-    private LocationRequest locationRequest;
     private Promise promise;
 
 
@@ -64,64 +60,20 @@ public class RNAndroidLocationEnablerModule extends ReactContextBaseJavaModule i
 
         this.promise = promise;
 
-        googleApiClient = new GoogleApiClient.Builder(getCurrentActivity())
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).build();
-        googleApiClient.connect();
-
-        locationRequest = LocationRequest.create();
+        LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(params.hasKey(LOCATION_INTERVAL_DURATION_PARAMS_KEY) ? params.getInt(LOCATION_INTERVAL_DURATION_PARAMS_KEY) : DEFAULT_INTERVAL_DURATION);
         locationRequest.setFastestInterval(params.hasKey(LOCATION_FAST_INTERVAL_DURATION_PARAMS_KEY) ? params.getInt(LOCATION_FAST_INTERVAL_DURATION_PARAMS_KEY) : DEFAULT_FAST_INTERVAL_DURATION);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        Task<LocationSettingsResponse> task = LocationServices.getSettingsClient(getCurrentActivity()).checkLocationSettings(builder.build());
+        task.addOnCompleteListener(this);
     }
 
     @Override
     public String getName() {
         return SELF_MODULE_NAME;
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
-        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
-        result.setResultCallback(this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
-        final Status status = locationSettingsResult.getStatus();
-        switch (status.getStatusCode()) {
-            case LocationSettingsStatusCodes.SUCCESS:
-                if (promise != null) promise.resolve("already-enabled");
-                this.promise = null;
-                break;
-            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                try {
-                    status.startResolutionForResult(getCurrentActivity(), REQUEST_CHECK_SETTINGS);
-                } catch (IntentSender.SendIntentException exception) {
-                    Log.e(TAG, "Failed to show dialog", exception);
-                    if (promise != null) promise.reject(ERR_FAILED_OPEN_DIALOG_CODE, new RNAndroidLocationEnablerException("Failed to show dialog", exception));
-                    this.promise = null;
-                }
-                break;
-            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                if (promise != null) promise.reject(ERR_SETTINGS_CHANGE_UNAVAILABLE_CODE, new RNAndroidLocationEnablerException("Settings change unavailable"));
-                this.promise = null;
-                break;
-        }
     }
 
     @Override
@@ -139,5 +91,34 @@ public class RNAndroidLocationEnablerModule extends ReactContextBaseJavaModule i
     @Override
     public void onNewIntent(Intent intent) {
 
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+        try {
+            task.getResult(ApiException.class);
+            if (promise != null) promise.resolve("already-enabled");
+            promise = null;
+        } catch (ApiException exception) {
+            switch (exception.getStatusCode()) {
+                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                    try {
+                        ResolvableApiException resolvable = (ResolvableApiException) exception;
+                        resolvable.startResolutionForResult(getCurrentActivity(), REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        Log.e(TAG, "Failed to show dialog", sendEx);
+                        if (promise != null) promise.reject(ERR_FAILED_OPEN_DIALOG_CODE, new RNAndroidLocationEnablerException("Failed to show dialog", sendEx));
+                        this.promise = null;
+                    } catch (ClassCastException classCast) {
+                        if (promise != null) promise.reject(ERR_INTERNAL_ERROR, new RNAndroidLocationEnablerException("Internal error", classCast));
+                        this.promise = null;
+                    }
+                    break;
+                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                    if (promise != null) promise.reject(ERR_SETTINGS_CHANGE_UNAVAILABLE_CODE, new RNAndroidLocationEnablerException("Settings change unavailable"));
+                    this.promise = null;
+                    break;
+            }
+        }
     }
 }
